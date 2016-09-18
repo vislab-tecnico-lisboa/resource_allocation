52e04ad3b09dd59ff12cbe2a56e12759406e45ad
Joao Avelino <joao.manuel.avelino@gmail.com>
2016-09-18 12:14:01 -0300

    scale fixed

--- lib/tracking/updateAssignedTracks.m
+++ lib/tracking/updateAssignedTracks.m
@@ -17,10 +17,21 @@ for i = 1:numAssignedTracks
     
     % Correct the estimate of the object's location
     % using the new detection.
-    correct(tracks(trackPosition).stateKalmanFilter, [centroid bbox(4)/bbox(3)]);
+    correct(tracks(trackPosition).stateKalmanFilter, [centroid bbox(4)/tracks(trackPosition).min_height]);
     
     % Replace predicted bounding box with detected
     % bounding box.
+    sc = tracks(trackPosition).stateKalmanFilter.State(3);
+    center = tracks(trackPosition).stateKalmanFilter.State(1:2);
+    min_height = tracks(trackPosition).min_height;
+    width = 40*sc;
+    height = min_height*sc;
+    
+    bbox(1) = center(1)-width/2;
+    bbox(2) = center(2)-height/2;
+    bbox(3) = width;
+    bbox(4) = height;
+    
     tracks(trackPosition).bbox = bbox;
     
     % Update track's age.
